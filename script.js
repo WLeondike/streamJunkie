a295d1af671d399d9232ac54f2fa736213fc0a48
Anthony Aguiar <michant1997@gmail.com>
2020-06-26 15:03:27 -0400

    updated a new proxy

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
         let movieYear = $("#movie_name").val();
         console.log(`Locals ${genreId} and ${movieYear}`);
         //Ajax
-        var queryURL = "https://api.themoviedb.org/3/discover/movie/?with_genres=" + genreId + "&primary_release_year=" + movieYear + "&api_key=52cc32f4af978457c9927f10c080f307&language=en-US";
+        var queryURL = "https://thingproxy.freeboard.io/fetch/https://api.themoviedb.org/3/discover/movie/?with_genres=" + genreId + "&primary_release_year=" + movieYear + "&api_key=52cc32f4af978457c9927f10c080f307&language=en-US";
         $.ajax({
             
             url: queryURL,
