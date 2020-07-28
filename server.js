88fd108141dea07bda1a62c0a515c0dcb6ecd65c
WLeondike <billyleondike@gmail.com>
2020-07-28 15:55:02 -0400

    added dependencies, working on server

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log(`app listening on: http://localhost:${PORT}`);
+});
